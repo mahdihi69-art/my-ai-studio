863f793445db173b3cd42fb1042b2983033c1c6c
mahdihi69-art <mahdihi69@gmail.com>
2026-09-13 19:00:03 +0330

    Fix Story Maker lightweight TTS compile constants

---
+++ story/src/main/java/com/mahdihi69/story/TextToSpeech.java
@@ -0,0 +1,8 @@
+package com.mahdihi69.story;
+
+/** Compatibility constants for the lightweight Android TTS implementation. */
+final class TextToSpeech {
+    private TextToSpeech() {}
+    static final int QUEUE_FLUSH = android.speech.tts.TextToSpeech.QUEUE_FLUSH;
+    static final int QUEUE_ADD = android.speech.tts.TextToSpeech.QUEUE_ADD;
+}
